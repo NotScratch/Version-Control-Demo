1ed139c4bd5bf9977a4dd63ea9ab1bb304d58eae
Derek <jones.derek@maranathastudents.org>
2021-02-12 16:29:38 -0800

    Video 10

--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -1,31 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Menus : MonoBehaviour
 {
-    public void Replay()
+    public string nextLevel;
+    /*public void Replay()
     {
-        FindObjectOfType<GameManager>().Reset();
-
-
-
-    }
-
+    FindObjectOfType<GameManager>().Reset();
+    }*/
     public void QuitGame()
     {
         Application.Quit();
-
-
-
     }
-
-
-
-
-
-
-
-
-
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
 }
