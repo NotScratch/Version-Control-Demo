4a746857d42b3d773a8b97aa2806baeabe0c8337
Derek <jones.derek@maranathastudents.org>
2021-03-16 11:15:56 -0700

    Video 16b

--- Assets/Scripts/EnemyHP.cs
+++ Assets/Scripts/EnemyHP.cs
@@ -9,6 +9,7 @@ public class EnemyHP : MonoBehaviour
     private bool isDead;
     private Collider2D parentCol;
     private Collider2D hurtboxCol;
+    private SpriteRenderer spriteRend;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,7 @@ public class EnemyHP : MonoBehaviour
         theAnim = transform.parent.GetComponent<Animator>();
         parentCol = transform.parent.GetComponent<Collider2D>();
         hurtboxCol = GetComponent<Collider2D>();
+        spriteRend = transform.parent.GetComponent<SpriteRenderer>();
     }
     // Update is called once per frame
     void Update()
@@ -33,10 +35,21 @@ public class EnemyHP : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHP -= damage;
+        StartCoroutine("HitConfirm");
     }
     IEnumerator KillSwitch()
     {
         yield return new WaitForSeconds(2f);
         Destroy(transform.parent.gameObject);
+    }
+    IEnumerator HitConfirm()
+    {
+        if (currentHP > 0)
+        {
+            spriteRend.enabled = false;
+            yield return new WaitForSeconds(0.1f);
+            spriteRend.enabled = true;
+
+        }
     }
 }
